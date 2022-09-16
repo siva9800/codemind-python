591d776d2b7c8e2a7dec21a4505759c6d537361f
SIVA POTHULA <106661571+siva9800@users.noreply.github.com>
2022-09-16 18:48:52 +0530

    n=input().lower() m=input().lower() n1=list(n) m1=list(m) v=[i for i in n1 if i!=' ' and i not in m1] v1=[i for i in m1 if i!=' ' and i not in n1] c=set(v+v1) c1=sorted(list(c)) print(''.join(c1))

---
+++ non_common_characters.py
@@ -0,0 +1,9 @@
+n=input().lower()
+m=input().lower()
+n1=list(n)
+m1=list(m)
+v=[i for i in n1 if i!=' ' and i not in m1]
+v1=[i for i in m1 if i!=' ' and i not in n1]
+c=set(v+v1)
+c1=sorted(list(c))
+print(''.join(c1))
