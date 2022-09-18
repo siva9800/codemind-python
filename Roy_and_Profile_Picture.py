b4508de27a08e9078d07e5045f39bbb50fb90f87
SIVA POTHULA <106661571+siva9800@users.noreply.github.com>
2022-09-18 15:45:54 +0530

    l=int(input()) for i in range(int(input())):     w,h=map(int,input().split())     if w<l or h<l:         print('UPLOAD ANOTHER')     else:         if w==h:             print("ACCEPTED")         else:             print('CROP IT')

---
+++ Roy_and_Profile_Picture.py
@@ -0,0 +1,10 @@
+l=int(input())
+for i in range(int(input())):
+    w,h=map(int,input().split())
+    if w<l or h<l:
+        print('UPLOAD ANOTHER')
+    else:
+        if w==h:
+            print("ACCEPTED")
+        else:
+            print('CROP IT')
