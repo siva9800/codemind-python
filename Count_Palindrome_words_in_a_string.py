2c8663cf062511f6b476b745b0750b53462302a5
SIVA POTHULA <106661571+siva9800@users.noreply.github.com>
2022-09-18 20:55:07 +0530

    def palin(n):     i=0     j=len(n)-1     while i<j:         if n[i]!=n[j]:             return False         i+=1         j-=1     return True c=0 for i in input().lower().split():     if palin(i):         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,6 +1,14 @@
-n=input().lower().split()
+def palin(n):
+    i=0
+    j=len(n)-1
+    while i<j:
+        if n[i]!=n[j]:
+            return False
+        i+=1
+        j-=1
+    return True
 c=0
-for i in n:
-    if i==i[::-1]:
+for i in input().lower().split():
+    if palin(i):
         c+=1
 print(c)
